abb7a7b8d5a8680825f5841dd49beaaf28a39daa
manjunathhalli <manjunathhalli629@gmail.com>
2021-08-13 13:36:16 +0530

    ability to add ratings to each hotel

--- com/bridgelabz/workshop/HotelReservationMain.java
+++ com/bridgelabz/workshop/HotelReservationMain.java
@@ -1,16 +1,30 @@
 package com.bridgelabz.workshop;
+import java.time.LocalDate;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 public class HotelReservationMain {
     public static void main(String[] args) {
         HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("LakeWood", 110, 90);
-        hotelReservation.addHotel("BridgeWood", 150, 50);
-        hotelReservation.addHotel("RidgeWood", 220, 150);
+        hotelReservation.addHotel("LakeWood", 110, 90, 3);
+        hotelReservation.addHotel("BridgeWood", 150, 50, 4);
+        hotelReservation.addHotel("RidgeWood", 220, 150, 5);
+        //list out hotel Details
         hotelReservation.getHotelDetails();
 
+        //giving start and end dates of the schedule
+        LocalDate dateStart = LocalDate.of(2021, Month.AUGUST, 11);
+        LocalDate dateEnd = LocalDate.of(2021, Month.AUGUST, 20);
+        System.out.println("Starting date : "+dateStart);
+        System.out.println("Ending date : "+dateEnd);
+        int noOfDays = (int) ChronoUnit.DAYS.between(dateStart, dateEnd);
+        System.out.println("Total days is : "+noOfDays);
+        //finding getWeekDaysCount
+        int weekEndsCount = hotelReservation.getWeekDaysCount(dateStart, noOfDays);
+        int weekDaysCount = noOfDays - weekEndsCount;
+
+        //find getMinPriceHotel
+        hotelReservation.getMinPriceHotel(weekEndsCount, weekDaysCount);
 
     }
 }
