9d01e06412b7b2aef1de9348f342d0c88863b734
manjunathhalli <manjunathhalli629@gmail.com>
2021-08-13 12:29:55 +0530

    Ability to add Hotel in a Hotel

---
+++ com/bridgelabz/workshop/HotelReservationMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.workshop;
+
+import java.util.*;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.viewHotels();
+
+    }
+}
